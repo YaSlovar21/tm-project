e8c283c351300999f960205b44898f70b3a3fb35
YaSlovar_work <stanko.tsep@yandex.ru>
2022-03-04 15:53:43 +0700

    04_03_2022

--- src/pages/service.js
+++ src/pages/service.js
@@ -1,1 +1,20 @@
 import './service.css'
+
+const formChoice = document.forms.formChoice;
+
+const plastTable = document.querySelector('.service-grid__plast-table');
+const sealsTable = document.querySelector('.service-grid__seals-table');
+
+function changeView(type) {
+
+}
+
+formChoice.addEventListener("change" , ()=>{
+  if (formChoice.elements.radio.value === 'plast') {
+    plastTable.style.display = 'block';
+    sealsTable.style.display = 'none';
+  } else {
+    plastTable.style.display = 'none';
+    sealsTable.style.display = 'block';
+  }
+});
