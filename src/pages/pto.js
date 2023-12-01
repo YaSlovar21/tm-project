8e25eb871678576e4d66416ee0f9298b32ae7fd8
YaSlovar_work <stanko.tsep@yandex.ru>
2023-12-01 16:29:34 +0700

    start-new-block-pto

--- src/pages/pto.js
+++ src/pages/pto.js
@@ -431,3 +431,24 @@ Array.from(document.querySelectorAll('.smooth-scroll-link')).map((item, index) =
     });
   })
 });
+
+const scrollFactoryContainer =  document.querySelector('.scroll-factory');
+
+const isScrollingFact = (evt) => {
+  let newScrollLeft = scrollFactoryContainer.scrollLeft + evt.deltaY;
+  console.log(newScrollLeft)
+  let deltaRight = scrollFactoryContainer.querySelector('.scroll-factory__inner').scrollWidth -scrollFactoryContainer.scrollLeft - scrollFactoryContainer.clientWidth  ;
+  console.log(deltaRight);
+  if ( newScrollLeft < -80 || (deltaRight < 20 && deltaRight > -1 && evt.deltaY >0) ) {
+    console.log(deltaRight );
+    return;
+  }
+  evt.preventDefault();
+  scrollFactoryContainer.scrollLeft += evt.deltaY;
+}
+
+scrollFactoryContainer.addEventListener("wheel", (evt) => {
+  console.log(scrollFactoryContainer.scrollLeft , scrollFactoryContainer.clientWidth, scrollFactoryContainer.querySelector('.scroll-factory__inner').scrollWidth)
+  isScrollingFact(evt);
+});
+
